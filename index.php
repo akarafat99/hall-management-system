7fb96d68bba71d2312b0ae3e0a9df4e9ae807980
akarafat99 <arafatabdulkhaled@gmail.com>
2025-03-07 03:18:44 +0600

    Arafat 7 march 2025

--- index.php
+++ index.php
@@ -72,67 +72,9 @@ if ($session->getObject('user') !== null) {
 </head>
 
 <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+    <!-- Include the navbar from a separate file -->
+    <?php include_once 'student/navbar-student.php'; ?>
 
-    <div class="site-mobile-menu site-navbar-target">
-        <div class="site-mobile-menu-header">
-            <div class="site-mobile-menu-close mt-3">
-                <span class="icon-close2 js-menu-toggle"></span>
-            </div>
-        </div>
-        <div class="site-mobile-menu-body"></div>
-    </div>
-
-    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
-
-        <div class="container-fluid">
-            <div class="d-flex align-items-center">
-                <div class="site-logo mr-auto w-20"><a href="index.html">MM HALL</a></div>
-
-                <div class="mx-auto text-center">
-                    <nav class="site-navigation position-relative text-right" role="navigation">
-                        <ul class="site-menu main-menu js-clone-nav mx-auto d-none d-lg-block  m-0 p-0">
-                            <li><a href="index.html" class="nav-link">Home</a></li>
-                            <li><a href="notice.html" class="nav-link">Notices</a></li>
-                            <li class="nav-item dropdown"><a href="" class="nav-link dropdown-toggle"
-                                    id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
-                                    aria-expanded="false">Apply</a>
-
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="noc.html">Hall NOC</a>
-                                    <a class="dropdown-item" href="seat-in-hall.html">Seat in hall</a>
-                                    <a class="dropdown-item" href="#">Seat change in the hall</a>
-                                </div>
-                            </li>
-                            <li><a href="#" class="nav-link">About Us</a></li>
-                            <li><a href="#" class="nav-link">Contact Us</a></li>
-                        </ul>
-                    </nav>
-                </div>
-
-                <div class="ml-auto w-20">
-                    <nav class="site-navigation position-relative text-right" role="navigation">
-                        <ul class="site-menu main-menu site-menu-dark js-clone-nav mr-auto d-none d-lg-block m-0 p-0">
-                            <!-- <li class="cta"> <a href="#" class="primary-button py-2 px-4">Dashboard</a></li> -->
-                            <li class="nav-item dropdown"><a href="" class="primary-button dropdown-toggle"
-                                    id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
-                                    aria-expanded="false">Dashboard</a>
-
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="Admin Dashboard/Dashbaord.html">View Profile</a>
-                                    <a class="dropdown-item" href="#">My Applications</a>
-                                    <a class="dropdown-item" href="#">JUST Wallet</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </nav>
-                    <a href="#"
-                        class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black float-right"><span
-                            class="icon-menu h3"></span></a>
-                </div>
-            </div>
-        </div>
-
-    </header>
 
     <div class="site-wrap">
         <div class="intro-section" id="home-section">
